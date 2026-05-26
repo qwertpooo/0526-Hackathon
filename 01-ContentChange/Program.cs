cd12e3d32c2c2e81552140fe7d65cdad50091a85
Lin Chang <peylin.tw@gmail.com>
2026-05-26 15:32:26 +0800

    加入專案檔案。

---
+++ 01-ContentChange/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01_ContentChange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個大於90的整數:");
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out int max) && max > 90)
+            {
+                // 使用 for 迴圈從 1 跑到 max
+                for (int i = 1; i <= max; i++)
+                {
+                    if (i % 15 == 0)
+                    {
+                        Console.WriteLine("Dann");
+                        
+                    }
+                    else if(i % 5 == 0)
+                    {
+                        Console.WriteLine("School");
+                    }
+                    else if(i % 3 == 0)
+                    {
+                        Console.WriteLine("Build");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
+
